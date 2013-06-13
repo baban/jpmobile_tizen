267dbc3bb4b7c311393d8459d69791f0fc5797de
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-06-13 21:17:47 +0900

    jpmobile_tizen support

--- lib/jpmobile_tizen.rb
+++ lib/jpmobile_tizen.rb
@@ -1,3 +1,6 @@
-require "jpmobile_tizen/version"
-require "jpmobile_tizen/jpmobile_tizen"
-require "jpmobile/mobile/tizen"
+module Jpmobile
+  module Mobile
+    autoload :Tizen, 'jpmobile/mobile/tizen'
+    DEFAULT_CARRIERS_WITH_TIZEN = %w(Docomo Au Softbank Vodafone Emobile Willcom Ddipocket Iphone Android WindowsPhone Tizen)
+  end
+end
